02df48543a8d7bdaca071025b2f4d8c37727986b
Thomas Simonini <simonini.thomas.pro@gmail.com>
2023-01-31 14:28:56 +0100

    Update setup.py

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -78,12 +78,15 @@ setup(
         "attrs>=19.3.0",
         'pypiwin32==223;platform_system=="Windows"',
         "importlib_metadata==4.4; python_version<'3.8'",
+        "huggingface_hub~=0.8"
     ],
     python_requires=">=3.8.13,<=3.10.8",
     entry_points={
         "console_scripts": [
             "mlagents-learn=mlagents.trainers.learn:main",
             "mlagents-run-experiment=mlagents.trainers.run_experiment:main",
+            "mlagents-push-to-hf=mlagents.utils.push_to_hf:main",
+            "mlagents-load-from-hf=mlagents.utils.load_from_hf:main"
         ],
         # Plugins - each plugin type should have an entry here for the default behavior
         ML_AGENTS_STATS_WRITER: [
